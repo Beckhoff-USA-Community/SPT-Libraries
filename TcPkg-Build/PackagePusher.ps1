0d81fb1fa795127cd0a1006ce4e59ff6cea73ca6
John Helfrich <john.j.helfrich@ieee.org>
2024-10-10 13:40:32 -0400

    Adjusting for Nexus

--- TcPkg-Build/PackagePusher.ps1
+++ TcPkg-Build/PackagePusher.ps1
@@ -1,7 +1,5 @@
-param($apikey)
-
 #Push the packages
 $packages = Get-ChildItem "build\"
 foreach ($package in $packages){
-    dotnet nuget push $package.FullName -s "https://community.beckhoff-usa.com" -k "private_key"
+    dotnet nuget push $package.FullName -s "https://packages.beckhoff-usa.com/repository/tcpkg-community/"
 }
